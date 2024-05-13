4941cef486df5f44e1c7ae790bbf0ce8f4d44dfe
mfonmfon <mfonm579@gmail.com>
2024-05-13 15:01:00 +0100

    commit

--- Practice/PracticeOne.js
+++ Practice/PracticeOne.js
@@ -1,15 +1,18 @@
-const prompt = require("prompt-sync")();
+Psudeo code for menstrual app
 
-function findingRunWayLength(){
+1. research on the female menstrual cycle
 
-	let length = 0;
-	let speed = prompt("Enter the speed");
-	let acceleration = prompt("Enter the acceleration");
-	
-	totalSpeed = speed * speed;
-	length = (totalSpeed  * acceleration);
+Questions:
 
-	console.log(length);
+When do girls have their period:
 
-}
-console.log(findingRunWayLength());
+1. The menstrual cycle is a term to describe the sequence of events that occur
+   in your body as it prepares for the possibility of pregnancy each month. 
+
+
+How Long Is  A Menstrual cyle last.
+
+1. The average length of a menstrual cycle is 28 days. However,
+   a cycle can range in length from 21 days to about 35 days and still be normal.
+
+2. Most people have their period (bleed) for between three and seven days.
