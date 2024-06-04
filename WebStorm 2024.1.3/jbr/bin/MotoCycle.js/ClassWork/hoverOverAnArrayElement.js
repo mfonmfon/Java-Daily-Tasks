00a3dace6b73a4260e8ce1025db0ea3fdeaee97c
mfonmfon <mfonm579@gmail.com>
2024-06-04 12:12:52 +0100

    commit

--- WebStorm 2024.1.3/jbr/bin/MotoCycle.js/ClassWork/hoverOverAnArrayElement.js
+++ WebStorm 2024.1.3/jbr/bin/MotoCycle.js/ClassWork/hoverOverAnArrayElement.js
@@ -1,2 +1,34 @@
-let firstArrays = [ "bola", "bella", "vine", "coka"]
+let firstArrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function evenNumber(numbers) {
+    let newNumbers = new Array(numbers);
+    for (let index = 0; index < numbers.length; index++) {
+        if (newNumbers[index] % 2 === 0) {
+            newNumbers.push(numbers[index]);
+            return newNumbers;
+        }
+    }
+    function anotherEvenNumber(numbers) {
+        let anotherArrays = []
+        for (let index = 0; index < numbers.length; index++) {
+            let isEven = numbers[index] % 2 === 0;
+            if (isEven) {
+                anotherArrays.push(numbers[index])
+                return anotherArrays
+            }
+        }
+        function useAnotherEvenNumbers(numbers) {
+            let arr = []
+            for (let index = 0; index < numbers.length; index++) {
+                let evenIndex = numbers[index] % 2 === 0
+                if (eveIndex) {
+                    numbers.shift(numbers)
+                    return arr
+                }
+            }
+        }
+        useAnotherEvenNumbers
+    }
+    anotherEvenNumber()
+}
+evenNumber()
